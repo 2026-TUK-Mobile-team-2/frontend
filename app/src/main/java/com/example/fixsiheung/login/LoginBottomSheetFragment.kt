92b6da43f2ac551615c78b56f3720c4dd7252be9
ha-neul2da <parkha6035@naver.com>
2026-05-17 01:15:59 +0900

    - 로그인화면 버튼 기능 추가 - 맵 화면으로 전환

--- app/src/main/java/com/example/fixsiheung/login/LoginBottomSheetFragment.kt
+++ app/src/main/java/com/example/fixsiheung/login/LoginBottomSheetFragment.kt
@@ -1,14 +1,18 @@
 package com.example.fixsiheung.login
 
+import android.content.Intent
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.fixsiheung.R
+import androidx.core.widget.addTextChangedListener
+import com.example.fixsiheung.MainMapActivity
 import com.example.fixsiheung.databinding.ActivityLoginBottomBinding
-import com.example.fixsiheung.databinding.ActivityMainMapBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import kotlin.jvm.java
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -29,18 +33,58 @@ class LoginBottomSheetFragment : BottomSheetDialogFragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding=ActivityLoginBottomBinding.inflate(inflater, container, false)
+        _binding = ActivityLoginBottomBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, tastes: Bundle?) {
         super.onViewCreated(view, tastes)
+        setButtonState(false)
 
+
+        binding.etId.addTextChangedListener {
+            checkInputs()
+        }
+
+        binding.etPassword.addTextChangedListener {
+            checkInputs()
+        }
         // 바텀 시트 내부의 로그인 버튼 클릭 이벤트 처리
         binding.btnBottomSheetLogin.setOnClickListener {
             //val email = binding.etEmail.text.toString()
             // 백엔드 로그인 API 호출 로직 연결부
-            dismiss() // 작업 끝나면 바텀 시트 닫기
+            val intent = Intent(requireContext(), MainMapActivity::class.java)
+            startActivity(intent)
+            activity?.finish()
+            dismiss()
+        }
+    }
+
+
+    // 아이디, 비입력 체크
+    private fun checkInputs() {
+        val id = binding.etId.text.toString().trim()
+        val password = binding.etPassword.text.toString().trim()
+
+        val isBothFilled = id.isNotEmpty() && password.isNotEmpty()
+
+        setButtonState(isBothFilled)
+    }
+
+    // 버튼 상태 변경
+    private fun setButtonState(isEnable: Boolean) {
+        binding.btnBottomSheetLogin.isEnabled = isEnable
+
+        if (isEnable) {
+            // 활성화 상태: 버튼 보라색, 글자 흰색
+            binding.btnBottomSheetLogin.backgroundTintList =
+                ColorStateList.valueOf(Color.parseColor("#8B00FF"))
+            binding.btnBottomSheetLogin.setTextColor(Color.WHITE)
+        } else {
+            // 비활성화 상태: 버튼 연회색, 글자 어두운 회색
+            binding.btnBottomSheetLogin.backgroundTintList =
+                ColorStateList.valueOf(Color.parseColor("#D1D5DB"))
+            binding.btnBottomSheetLogin.setTextColor(Color.parseColor("#8E94A0"))
         }
     }
 
