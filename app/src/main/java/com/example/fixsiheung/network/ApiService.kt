fd3cb25e99a9124c677bd16ce259fdc56313f932
minjae <blockman1346@gmail.com>
2026-05-26 16:28:27 +0900

    이미지 업로드 api 추가

--- app/src/main/java/com/example/fixsiheung/network/ApiService.kt
+++ app/src/main/java/com/example/fixsiheung/network/ApiService.kt
@@ -4,10 +4,14 @@ import com.example.fixsiheung.model.LoginRequest
 import com.example.fixsiheung.model.LoginResponse
 import com.example.fixsiheung.model.Report
 import com.example.fixsiheung.model.SignupRequest
+import okhttp3.MultipartBody
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Multipart
 import retrofit2.http.POST
+import retrofit2.http.Part
+import retrofit2.http.Path
 
 interface ApiService {
     // 민원 목록 가져오기
@@ -37,5 +41,12 @@ interface ApiService {
     // 이메일 중복 체크
     @POST("api/users/check-email")
     fun checkEmail(@Body request: Map<String, String>): Call<Map<String, Any>>
+
+    // 이미지 파일 전송을 위한 Multipart 설정
+    @Multipart
+    @POST("api/complaints/{complaint_id}/image")
+    fun uploadComplaintImage(
+        @Path("complaint_id") complaintId: Int,
+        @Part image: MultipartBody.Part): Call<Map<String, Any>>
 
 }
